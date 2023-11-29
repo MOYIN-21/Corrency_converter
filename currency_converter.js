eeb8b8fe7b3061ac8430b1d772c88167825d6cc8
Jesemeil <otahaghogho@gmail.com>
2023-11-29 17:16:00 +0100

    modified

--- currency_converter.js
+++ currency_converter.js
@@ -1,41 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sourceCurrencySelect = document.getElementById("sourceCurrency");
-    const targetCurrencySelect = document.getElementById("targetCurrency");
-    const amountInput = document.getElementById("amount");
+    const fromCurrencySelect = document.getElementById("From");
+    const toCurrencySelect = document.getElementById("To");
+    const amountInput = document.getElementById("textbox");
     const convertBtn = document.getElementById("convertBtn");
-    const resultDiv = document.getElementById("result");
 
-    
-    fetch("https://open.er-api.com/v6/latest?apikey=2559cdb3593580382aa8d0ae")
-        .then(response => response.json())
-        .then(data => {
-            const currencies = Object.keys(data.rates);
+    // Fetch currency data from the API
+    fetch("https://v6.exchangerate-api.com/v6/62f01a18eafd1efc8faf2af7/latest/USD")
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.result === "success") {
+                const currencies = Object.keys(data.conversion_rates);
 
-            currencies.forEach(currency => {
-                const option = document.createElement("option");
-                option.value = currency;
-                option.textContent = currency;
-                sourceCurrencySelect.appendChild(option.cloneNode(true));
-                targetCurrencySelect.appendChild(option.cloneNode(true));
-            });
-        });
+                // Populate dropdown menus with currency abbreviations
+                currencies.forEach((currency) => {
+                    const option1 = document.createElement("option");
+                    option1.value = currency;
+                    option1.text = currency;
+                    fromCurrencySelect.add(option1);
 
-    
-    convertBtn.addEventListener("click", function () {
-        const sourceCurrency = sourceCurrencySelect.value;
-        const targetCurrency = targetCurrencySelect.value;
-        const amount = amountInput.value;
+                    const option2 = document.createElement("option");
+                    option2.value = currency;
+                    option2.text = currency;
+                    toCurrencySelect.add(option2);
+                });
 
-        
-        fetch(`https://open.er-api.com/v6/convert?apikey=2559cdb3593580382aa8d0ae&from=${sourceCurrency}&to=${targetCurrency}&amount=${amount}`)
-            .then(response => response.json())
-            .then(data => {
-                const convertedAmount = data.result.toFixed(2);
-                resultDiv.textContent = `Converted Amount: ${convertedAmount} ${targetCurrency}`;
-            })
-            .catch(error => {
-                resultDiv.textContent = "Error fetching conversion data.";
-                console.error(error);
-            });
-    });
+                // Event listener for the "Convert" button
+                convertBtn.addEventListener("click", function () {
+                    const fromCurrency = fromCurrencySelect.value;
+                    const toCurrency = toCurrencySelect.value;
+                    const amount = parseFloat(amountInput.value);
+
+                    if (!fromCurrency || !toCurrency || isNaN(amount)) {
+                        alert("Please fill in all the fields with valid values.");
+                        return;
+                    }
+
+                    // Check if the selected currencies are present in the conversion rates
+                    if (!data.conversion_rates[fromCurrency] || !data.conversion_rates[toCurrency]) {
+                        alert("Invalid currency selection.");
+                        return;
+                    }
+
+                    // Calculate the converted amount
+                    const conversionRate = data.conversion_rates[toCurrency] / data.conversion_rates[fromCurrency];
+                    const convertedAmount = amount * conversionRate;
+
+                    // Display the result
+                    const resultParagraph = document.createElement("p");
+                    resultParagraph.textContent = `${amount} ${fromCurrency} is equal to ${convertedAmount.toFixed(2)} ${toCurrency}`;
+                    document.body.appendChild(resultParagraph);
+                });
+            } else {
+                console.error("Error fetching currency data:", data.result);
+            }
+        })
+        .catch((error) => console.error("Error fetching currency data:", error));
 });
